61b7b2e2e49db429e457ef63d14d7deee5e0aeaf
kisslaci98 <klaci9898@gmail.com>
2020-02-10 11:42:23 +0100

    1.kommit

---
+++ Fuvar/Fuvar/fuvarInfo.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fuvar
+{
+    class fuvarInfo
+    {
+        public int Azonosito { get; set; }
+        public string Idobelyegzo { get; set; }
+        public int Idotartam { get; set; }
+        public double MegtettTavolsag { get; set; }
+        public double Viteldij { get; set; }
+        public double Borravalo { get; set; }
+        public string FizetesModja { get; set; }
+
+        public fuvarInfo(int az, string ib, int it, double mt, double vt, double bv, string fm)
+        {
+            this.Azonosito = az;
+            this.Idobelyegzo = ib;
+            this.Idotartam = it;
+            this.MegtettTavolsag = mt;
+            this.Viteldij = vt;
+            this.Borravalo = bv;
+            this.FizetesModja = fm;
+        }
+
+        public fuvarInfo()
+        {
+
+        }
+        public fuvarInfo(string ib)
+        {
+            this.Idobelyegzo = ib;
+        }
+        public string kiiroString()
+        {
+            return $"{this.Azonosito};{this.Idobelyegzo};{this.Idotartam};{this.MegtettTavolsag};{this.Viteldij};{this.Borravalo};{this.FizetesModja}";
+        }
+    }
+}
